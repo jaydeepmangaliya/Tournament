a0921bdaf90f3d86e480098b47642536b3619795
jaydeepmangaliya <123927153+jaydeepmangaliya@users.noreply.github.com>
2025-05-31 11:03:29 +0530

    admin

--- BE/index.js
+++ BE/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(express.static(path.join(__dirname, "FE/dist")));
 require('./routes/slotes-route')(app);
 require('./routes/auth-route')(app);
 require('./routes/user-route')(app)
